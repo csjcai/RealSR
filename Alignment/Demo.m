9a9bf44d904baab26bfb7e5ca05b67db829a87ec
Cai Jianrui <34891158+csjcai@users.noreply.github.com>
2019-08-02 19:50:04 +0800

    Add files via upload

--- Alignment/Demo.m
+++ Alignment/Demo.m
@@ -10,14 +10,14 @@ for i = 1:2:size(filepaths)
 
     s = 2;                                             % s = len1/len2, len1&lend2 are the focal length of captured image (len1>len2)
     r = 1 - 1/s;                                       % Scale
-    I2_zoom=warpImg(I2,[-r,0,0,0]);
+    I2_zoom =w arpImg(I2,[-r,0,0,0]);
     
-    tau0=zeros(6,1);
-    iter=3;                                            % number of iterations
+    tau0 = zeros(6,1);
+    iter = 3;                                          % number of iterations
+
     [I2_t,tau] = align_l(I2_zoom,I1,tau0,iter);
-    
-    [I2_t_l]=luminance_transfer(I1,I2_t);              % transfer luminance
-    [I2_t_c]=color_transfer(I1,I2_t);                  % transfer color
+    [I2_t_l] = luminance_transfer(I1,I2_t);            % transfer luminance
+    [I2_t_c] = color_transfer(I1,I2_t);                % transfer color
 
     % where you aim to save the image
     imwrite(I1, ['./.', filepaths(i).name])            
