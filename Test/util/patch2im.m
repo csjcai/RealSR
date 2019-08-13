507b9204924f7649432ff4948c67e2c27d206066
Cai Jianrui <34891158+csjcai@users.noreply.github.com>
2019-08-13 17:48:08 +0800

    Update patch2im.m

--- Test/util/patch2im.m
+++ Test/util/patch2im.m
@@ -1,15 +1,3 @@
-% function [img] = patch2im(patch, size_img, size_patch, size_skip)
-% 
-% img = zeros(size_img);
-% w = zeros(size_img);
-% patch_loc = patchLocation(size_img, size_patch, size_skip);
-% 
-% for n=1:size(patch_loc,3)
-%     img(patch_loc(:,:,n)) = img(patch_loc(:,:,n)) + patch(:,:,n);
-%     w(patch_loc(:,:,n)) = w(patch_loc(:,:,n)) + 1;
-% end
-% img = img ./ w;
-
 function [img] = patch2im(patch, size_img, size_patch, size_skip)
 
 img = zeros(size_img);
